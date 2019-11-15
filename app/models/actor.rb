a0eda62a6c97ef89ffdc7be7cad7d52c467366d9
Riley Moore <rilemoore@gmail.com>
2019-11-15 01:05:05 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+  
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+  
+  def list_roles
+    characters.map do |character|
+      if(character.actor_id == self.id)
+        "#{character.name} - #{shows[character.show_id - 1].name}"
+      end
+    end
+  end
   
 end
