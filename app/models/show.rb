7608ab30b0d2b234b79888efbdae697eac461859
Riley Moore <rilemoore@gmail.com>
2019-11-15 01:00:35 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,18 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+  
+  def actors_list
+    characters.map do |character|
+      if(character.show_id == self.id)
+        actors[character.actor_id - 1].full_name
+      end
+    end
+  end
+  
+  def build_network
+    
+  end
   
 end
